6131a160429cf13ba55cb89d276206f7312ce76d
Crowdbotics <team@crowdbotics.com>
2022-04-25 19:59:08 +0000

    Update admin title

--- ausocial_34928/urls.py
+++ ausocial_34928/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "AuSocial"
-admin.site.site_title = "AuSocial Admin Portal"
-admin.site.index_title = "AuSocial Admin"
+admin.site.site_header = "Au Social"
+admin.site.site_title = "Au Social Admin Portal"
+admin.site.index_title = "Au Social Admin"
 
 # swagger
 api_info = openapi.Info(
